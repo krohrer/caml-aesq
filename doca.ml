2007330c43b44466c0775c6be3ace2c2e75d3ab2
Kaspar M. Rohrer <kaspar.rohrer@gmail.com>
2010-03-18 04:33:00 +0100

    ANSI printer needs flush_queue implemented.

--- doca.ml
+++ doca.ml
@@ -92,26 +92,43 @@ let print pr doc = pr doc
 
 module ANSI =
 struct
-  type ansiop = [ `nop
-  ]
-
-  type simpleop = [
-  | `frag of string
-  | `ansi of ansiop
-  ]
-
   type op = [
-  | simpleop
+  | `push_intensity of intensity
+  | `push_justify of justify
+  | `push_underline of bool
+  | `push_inverted of bool
+  | `push_foreground of color
+  | `push_background of color
+  | `pop_intensity
+  | `pop_underline
+  | `pop_inverted
+  | `pop_foreground
+  | `pop_background
+  | `frag of string
   | `break
   | `withctx of context * op Stream.t
   ]
 
+  and elem = [
+  | `frag of string
+  | `intensity of intensity
+  | `justify of justify
+  | `underline of bool
+  | `inverted of bool
+  | `foreground of color
+  | `background of color
+  ]
+
   and color = [
-    `black | `red | `green | `yellow | `blue | `magenta | `cyan | `white | `default | `high of color
+    `black | `red | `green | `yellow | `blue | `magenta | `cyan | `white | `default
   ]
 
   and intensity = [
     `bold | `faint | `normal
+  ]
+
+  and justify = [
+    `left | `center | `right | `blco
   ]
 
   and context = {
@@ -119,6 +136,7 @@ struct
     c_width : int;
     c_depth : int;
     mutable c_intensity : intensity list;
+    mutable c_justify : justify list;
     mutable c_underline : bool list;
     mutable c_inverted : bool list;
     mutable c_foreground : color list;
@@ -126,10 +144,12 @@ struct
   }
 
   and printer = {
+    p_outc : out_channel;
     mutable p_context : context;
     mutable p_column : int;
-    mutable p_queue_count : int;
-    p_queue : simpleop Queue.t
+    mutable p_queue_columns : int;
+    mutable p_queue_fragments : int;
+    p_queue : elem Queue.t
   }
 
   (*------------------------------------*)
@@ -139,6 +159,7 @@ struct
     let make 
 	?(offset=0) ?(width=78) ?(depth=0)
 	?(intensity=`normal)
+	?(justify=`left)
 	?(underline=false)
 	?(inverted=false)
 	?(foreground=`default)
@@ -148,6 +169,7 @@ struct
 	  c_width = width;
 	  c_depth = depth;
 	  c_intensity = [intensity];
+	  c_justify = [justify];
 	  c_underline = [underline];
 	  c_inverted = [inverted];
 	  c_foreground = [foreground];
@@ -165,65 +187,241 @@ struct
     let width ctx = ctx.c_width
     let depth ctx = ctx.c_depth
 
-    let intensity ctx  = List.tl ctx.c_intensity
-    let underlines ctx = List.tl ctx.c_underline
-    let inverted ctx   = List.tl ctx.c_inverted
-    let foreground ctx = List.tl ctx.c_foreground
-    let background ctx = List.tl ctx.c_background
+    let intensity ctx  = List.hd ctx.c_intensity
+    let justify ctx    = List.hd ctx.c_justify
+    let underline ctx  = List.hd ctx.c_underline
+    let inverted ctx   = List.hd ctx.c_inverted
+    let foreground ctx = List.hd ctx.c_foreground
+    let background ctx = List.hd ctx.c_background
 
     let push_intensity ctx i  = ctx.c_intensity  <- i::ctx.c_intensity
+    let push_justify ctx j    = ctx.c_justify    <- j::ctx.c_justify
     let push_underline ctx f  = ctx.c_underline  <- f::ctx.c_underline
     let push_inverted ctx f   = ctx.c_inverted   <- f::ctx.c_inverted
     let push_foreground ctx c = ctx.c_foreground <- c::ctx.c_foreground
-    let push_foreground ctx c = ctx.c_foreground <- c::ctx.c_foreground
+    let push_background ctx c = ctx.c_background <- c::ctx.c_background
 
-    let pop_intensity ctx  = ctx.c_intensity  <- List.tl ctx.c_intensity
-    let pop_underline ctx  = ctx.c_underline  <- List.tl ctx.c_underline
-    let pop_inverted ctx   = ctx.c_inverted   <- List.tl ctx.c_inverted
-    let pop_foreground ctx = ctx.c_foreground <- List.tl ctx.c_foreground
-    let pop_background ctx = ctx.c_background <- List.tl ctx.c_background
+    let pop_intensity ctx = 
+      match ctx.c_intensity with
+	| [] -> failwith "ANSI.Context.pop_intensity"
+	| x::rest -> ctx.c_intensity <- rest; x
+    let pop_justify ctx =
+      match ctx.c_justify with
+	| [] -> failwith "ANSI.Context.pop_justify"
+	| x::rest -> ctx.c_justify <- rest; x
+    let pop_underline ctx =
+      match ctx.c_underline with
+	| [] -> failwith "ANSI.Context.pop_underline"
+	| x::rest -> ctx.c_underline <- rest; x
+    let pop_inverted ctx =
+      match ctx.c_inverted with
+	| [] -> failwith "ANSI.Context.pop_inverted"
+	| x::rest -> ctx.c_inverted <- rest; x
+    let pop_foreground ctx =
+      match ctx.c_foreground with
+	| [] -> failwith "ANSI.Context.pop_foreground"
+	| x::rest -> ctx.c_foreground <- rest; x
+    let pop_background ctx =
+      match ctx.c_background with
+	| [] -> failwith "ANSI.Context.pop_background"
+	| x::rest -> ctx.c_background <- rest; x
   end
 
   (*------------------------------------*)
 
   module Printer =
   struct
-    let make ctx = {
+    module C = Context
+
+    let make ctx outc = {
+      p_outc = outc;
       p_context = ctx;
       p_column = 0;
-      p_queue_count = 0;
+      p_queue_fragments = 0;
+      p_queue_columns = 0;
       p_queue = Queue.create ()
     }
 
-    let queue_frag pr (`frag s as f) =
-      pr.p_queue_count <- pr.p_queue_count + (String.length s);
+    let print_string pr s =
+      output_string pr.p_outc s
+
+    let queue_add_fragment pr (`frag s as f) =
+      pr.p_queue_fragments <- pr.p_queue_fragments + 1;
+      pr.p_queue_columns <- pr.p_queue_columns + (String.length s);
       Queue.add f pr.p_queue
 
-    let queue_ansi pr (`ansi o as a) =
-      Queue.add a pr.p_queue
+    let queue_add pr elem =
+      Queue.add elem pr.p_queue
 
     let flush_queue pr ctx =
+      (*  *)
       if ctx.c_depth = 0 then
-	()
+	output_string pr.p_outc "\n"
+ 
+    let print_esc pr code =
+      Printf.fprintf pr.p_outc "\x1b[%dm" code
+
+    let print_intensity pr =
+      function
+	| `bold   -> print_esc pr 1 
+	| `normal -> print_esc pr 22
+	| `faint  -> print_esc pr 2
+
+    let print_underline pr =
+      function
+	| true -> print_esc pr 4
+	| false -> print_esc pr 24
+
+    let print_inverted pr =
+      function
+	| true -> print_esc pr 7
+	| false -> print_esc pr 27
+
+    let print_foreground pr =
+      function
+	| `black ->
+	    print_esc pr 30
+	| `red ->
+	    print_esc pr 31
+	| `green ->
+	    print_esc pr 32
+	| `yellow -> 
+	    print_esc pr 33
+	| `blue -> 
+	    print_esc pr 34
+	| `magenta ->
+	    print_esc pr 35
+	| `cyan ->
+	    print_esc pr 36
+	| `white ->
+	    print_esc pr 37
+	| `default ->
+	    print_esc pr 39
+
+    let print_background pr =
+      function
+	| `black ->
+	    print_esc pr 40
+	| `red ->
+	    print_esc pr 41
+	| `green ->
+	    print_esc pr 42
+	| `yellow -> 
+	    print_esc pr 43
+	| `blue -> 
+	    print_esc pr 44
+	| `magenta ->
+	    print_esc pr 45
+	| `cyan ->
+	    print_esc pr 46
+	| `white ->
+	    print_esc pr 47
+	| `default ->
+	    print_esc pr 49
+
+    let print_ansi pr =
+      function
+	| `intensity i -> 
+	    print_intensity pr i
+	| `underline f ->
+	    print_underline pr f
+	| `inverted f ->
+	    print_inverted pr f
+	| `foreground c ->
+	    print_foreground pr c
+	| `background c ->
+	    print_background pr c
 
     let switch_context pr ctx =
-      if pr.p_context != ctx then begin
-	()
-      end
-	
+      let prctx = pr.p_context in
+	if prctx != ctx then begin
+	  begin let i = C.intensity ctx in
+	    if i <> C.intensity prctx then
+	      print_intensity pr i
+	  end;
+	  begin let f = C.underline ctx in
+	    if f <> C.underline prctx then
+	      print_underline pr f
+	  end;
+	  begin let f = C.inverted ctx in
+	    if f <> C.inverted prctx then
+	      print_inverted pr f
+	  end;
+	  begin let c = C.foreground ctx in
+	    if c <> C.foreground prctx then
+	      print_foreground pr c
+	  end;
+	  begin let c = C.background ctx in
+	    if c <> C.background prctx then
+	      print_background pr c
+	  end
+	end
+
     let rec print' pr ctx stream =
       let consume =
 	function
-	  | `frag s as f ->
-	      switch_context pr ctx;
-	      queue_frag pr f
-	  | `ansi o as a ->
-	      switch_context pr ctx;
-	      queue_ansi pr a
+	  | `frag _ as f ->
+	      queue_add_fragment pr f
+	  | `push_intensity i ->
+	      let i' = C.intensity ctx in
+		if i <> i' then
+		  queue_add pr (`intensity i);
+		C.push_intensity ctx i
+	  | `push_justify j ->
+	      let j' = C.justify ctx in
+		if j <> j' then
+		  queue_add pr (`justify j);
+		C.push_justify ctx j
+	  | `push_underline f ->
+	      let f' = C.underline ctx in
+		if f <> f' then
+		  queue_add pr (`underline true);
+		C.push_underline ctx f
+	  | `push_inverted f   ->
+	      let f' = C.inverted ctx in
+		if f <> f' then
+		  queue_add pr (`inverted true);
+		C.push_underline ctx f
+	  | `push_foreground c ->
+	      let c' = C.foreground ctx in
+		if c <> c' then 
+		  queue_add pr (`foreground c);
+		C.push_foreground ctx c
+	  | `push_background c ->
+	      let c' = C.background ctx in
+		if c <> c' then queue_add pr (`background c);
+		C.push_background ctx c
+	  | `pop_intensity ->
+	      let i' = C.pop_intensity ctx in
+	      let i = C.intensity ctx in
+		if i <> i' then 
+		  queue_add pr (`intensity i)
+	  | `pop_underline ->
+	      let f' = C.pop_underline ctx in
+	      let f = C.underline ctx in
+		if f <> f' then 
+		  queue_add pr (`underline f)
+	  | `pop_inverted ->
+	      let f' = C.pop_inverted ctx in
+	      let f = C.inverted ctx in
+		if f <> f' then 
+		  queue_add pr (`inverted f)
+	  | `pop_foreground ->
+	      let c' = C.pop_foreground ctx in
+	      let c = C.foreground ctx in
+		if c <> c' then 
+		  queue_add pr (`foreground c)
+	  | `pop_background ->
+	      let c' = C.pop_foreground ctx in
+	      let c = C.foreground ctx in
+		if c <> c' then 
+		  queue_add pr (`background c)
 	  | `break -> 
 	      flush_queue pr ctx
-	  | `withctx (ctx, stream) ->
-	      print' pr ctx stream
+	  | `withctx (ctx', stream) ->
+	      switch_context pr ctx';
+	      print' pr ctx' stream;
+	      switch_context pr ctx 
       in
 	Stream.iter consume stream
   end
